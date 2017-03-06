af227f6abe72987ce27b331688e4ca01b90be77f
kamono-mafuyu <kamono-mafuyu@dmm.com>
2017-03-06 18:52:13 +0900

    add: power-assert

--- test/math.js
+++ test/math.js
@@ -2,14 +2,14 @@
  * Created by mafuyuk on 2017/03/05.
  */
 
-import assert from "assert"
+import assert from "power-assert"
 import Add from "../src/math.js"
 
 describe("math", () => {
 
     describe(".add(x, y)", () => {
         it("should return 3 when given 1 and 2", () => {
-            assert.strictEqual(Add(1, 2), 3);
+            assert.equal(Add(1, 2), 3);
         });
     });
 });
