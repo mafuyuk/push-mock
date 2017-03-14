20b4c07384ecaa84ab9a4bb001ae46ab603a6438
mafuyuk <mafuyuk.m@gmail.com>
2017-03-15 00:53:41 +0900

    fix: modularization

--- src/index.js
+++ src/index.js
@@ -5,33 +5,37 @@ import WebPush from './webpush';
 
 WebPush.requestPushEnable();
 
-navigator.serviceWorker.register('./sw.js')
-    .then(navigator.serviceWorker.ready)
-    .then((result) => {
-        // 通知購読要求
-        const serverKey = 'BIXmkQhnr0Nobbo1ESQLo0gdyEzi7Z7YCc5onF3eXKwIAoqkaucz9USV3gJmx2LA5qFooAtM21ZhDrOZHFfPew8';
-        const serverKeyBin = WebPush.urlsafeB64toBin(serverKey);
+function runWebPush() {
+    navigator.serviceWorker.register('./sw.js')
+        .then(navigator.serviceWorker.ready)
+        .then((result) => {
+            // 通知購読要求
+            const serverKey = 'BIXmkQhnr0Nobbo1ESQLo0gdyEzi7Z7YCc5onF3eXKwIAoqkaucz9USV3gJmx2LA5qFooAtM21ZhDrOZHFfPew8';
+            const serverKeyBin = WebPush.urlsafeB64toBin(serverKey);
 
-        return result.pushManager.subscribe({
-            userVisibleOnly: true,
-            applicationServerKey: serverKeyBin,
-        });
-    })
-    .then((subscription) => {
-        // エンドポイントなどの取得
-        const subscriptionInfo = JSON.stringify(subscription);
+            return result.pushManager.subscribe({
+                userVisibleOnly: true,
+                applicationServerKey: serverKeyBin,
+            });
+        })
+        .then((subscription) => {
+            // エンドポイントなどの取得
+            const subscriptionInfo = JSON.stringify(subscription);
 
-        fetch(
-            '/api/push/subscribe',
-            {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
+            fetch(
+                '/api/push/subscribe',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: subscriptionInfo,
                 },
-                body: subscriptionInfo,
-            },
-        );
-    })
-    .catch((e) => {
-        throw e;
-    });
+            );
+        })
+        .catch((e) => {
+            throw e;
+        });
+}
+
+runWebPush();
