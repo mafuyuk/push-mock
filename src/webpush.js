d0b01dad6eef2652cb2945f644daf22a2281d62d
mafuyuk <mafuyuk.m@gmail.com>
2017-03-16 00:28:52 +0900

    fix: sw path

--- src/webpush.js
+++ src/webpush.js
@@ -24,7 +24,7 @@ export default class WebPush {
                 reject('ServiceWorker is not available');
             }
 
-            navigator.serviceWorker.register('/sw.js', {
+            navigator.serviceWorker.register('./sw.js', {
                 scope: '/',
             }).then((registration) => {
                 resolve(registration.serviceWorker.ready);
