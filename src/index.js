c1c2b32b54f094da39892cdbc8d55578125a951b
mafuyuk <mafuyuk.m@gmail.com>
2017-03-17 20:33:31 +0900

    fix: use EventEmmiter2 as an object

--- src/index.js
+++ src/index.js
@@ -5,51 +5,48 @@ import EventEmitter2 from 'eventemitter2';
 
 import WebPush from './webpush';
 
-export default class RunWebPush extends EventEmitter2 {
-    constructor() {
-        super();
-        this.subscribe();
-    }
+function subscribe() {
+    return WebPush.requestPushEnable()
+        .then(WebPush.registerServiceWorker)
+        .then((result) => {
+            // 通知購読要求
+            const serverKey = 'BCb37fgHp86W2ilc4qPoDVbisxLw3WQ6mHM5kCcNiQC-5qb80uIYFW2QvZh4MW7PjE0JM2AjNigNBVgGl05LW5E';
+            const serverKeyBin = WebPush.urlsafeB64toBin(serverKey);
 
-    subscribe() {
-        WebPush.requestPushEnable()
-            .then(WebPush.registerServiceWorker)
-            .then((result) => {
-                // 通知購読要求
-                const serverKey = 'BCb37fgHp86W2ilc4qPoDVbisxLw3WQ6mHM5kCcNiQC-5qb80uIYFW2QvZh4MW7PjE0JM2AjNigNBVgGl05LW5E';
-                const serverKeyBin = WebPush.urlsafeB64toBin(serverKey);
-
-                return result.pushManager.subscribe({
-                    userVisibleOnly: true,
-                    applicationServerKey: serverKeyBin,
-                });
-            })
-            .then((subscription) => {
-                // エンドポイントなどの取得
-                const subscriptionInfo = JSON.stringify(subscription);
-
-                fetch(
-                    '/api/push/subscribe',
-                    {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                        body: subscriptionInfo,
-                    },
-                );
-                this.emit('success');
-            })
-            .catch((e) => {
-                this.emit('error', e);
+            return result.pushManager.subscribe({
+                userVisibleOnly: true,
+                applicationServerKey: serverKeyBin,
             });
-    }
+        })
+        .then((subscription) => {
+            // エンドポイントなどの取得
+            const subscriptionInfo = JSON.stringify(subscription);
+
+            fetch(
+                '/api/push/subscribe',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: subscriptionInfo,
+                },
+            );
+        })
+        .catch(e => Promise.reject(e));
 }
 
-console.log('RunWebPush');
-const ev = new RunWebPush();
-ev.on('error', (e) => {
-    console.log(`ERROR: ${e}`);
-}).on('success', () => {
-    console.log('SUCCESS');
-});
+function runWebPush() {
+    const ev = new EventEmitter2();
+
+    subscribe()
+        .then(ev.emit('success'))
+        .catch((e) => {
+            ev.emit('error', e);
+        });
+
+    return ev;
+}
+
+window.runWebPush = runWebPush;
+
